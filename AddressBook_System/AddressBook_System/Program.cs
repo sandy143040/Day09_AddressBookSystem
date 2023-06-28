34ad600e30abd731bd4ee2032dea3e1b15ec7c0a
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-29 03:27:37 +0530

    [Sandeep] Add: Edit contact person

--- AddressBook_System/AddressBook_System/Program.cs
+++ AddressBook_System/AddressBook_System/Program.cs
@@ -15,7 +15,7 @@ namespace AddressBook_System
 
             while (true)
             {
-                Console.WriteLine("Enter the Number of contact list you want add in AddressBook");
+                Console.WriteLine("Enter the Number of Contacts list you want add in AddressBook:");
                 int input = Convert.ToInt32(Console.ReadLine());
                 for (int index = 0; index < input; index++)
                 {
@@ -65,6 +65,61 @@ namespace AddressBook_System
                     foreach (Contacts contactList in addressBook)
                     {
                         contactList.DisplayContactInfo();
+                    }
+                }
+                Console.WriteLine("Edit by using First name");
+                string name = Console.ReadLine();
+                foreach(Contacts data in addressBook)
+                {
+                    if (data.firstName == name)
+                    {
+                        Console.WriteLine("Enter option to edit:\n1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Phone Number\n7.Email\n8.Exit");
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+
+                        {
+                            case 1:
+                                Console.WriteLine("Enter first name ");
+                                data.firstName = Console.ReadLine();
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter last name ");
+                                data.lastName = Console.ReadLine();
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter address");
+                                data.address = Console.ReadLine();
+                                break;
+                            case 4:
+                                Console.WriteLine("Enter city");
+                                data.city = Console.ReadLine();
+                                break;
+                            case 5:
+                                Console.WriteLine("Enter state");
+                                data.state = Console.ReadLine();
+                                break;
+                            case 6:
+                                Console.WriteLine("Enter phone number");
+                                data.phoneNumber = Console.ReadLine();
+                                break;
+                            case 7:
+                                Console.WriteLine("Enter email");
+                                data.email = Console.ReadLine();
+                                break;
+                            case 8:
+                                Console.WriteLine("Exiting the program....");
+                                break;
+                            default:
+                                Console.WriteLine("Entered Invalid Option");
+                                break;
+                        }
+                        Console.WriteLine("After Edit contact: \n");
+                        data.DisplayContactInfo();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Given first name is not found");
+                        return;
                     }
                 }
                 Console.ReadLine();
