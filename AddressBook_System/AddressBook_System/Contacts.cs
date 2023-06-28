4ab64489940ebdd50987f50a943ba533bcba603d
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-29 01:03:07 +0530

    [Sandeep] Add: Create a contact

---
+++ AddressBook_System/AddressBook_System/Contacts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_System
+{
+    public class Contacts
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+
+        public void DisplayContactInfo()
+        {
+            Console.WriteLine("Contact Information:");
+            Console.WriteLine($"First Name: {firstName}");
+            Console.WriteLine($"Last Name: {lastName}");
+            Console.WriteLine($"Address: {address}");
+            Console.WriteLine($"City: {city}");
+            Console.WriteLine($"State: {state}");
+            Console.WriteLine($"ZIP: {zip}");
+            Console.WriteLine($"Phone Number: {phoneNumber}");
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AddressBook_System/AddressBook_System/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_System
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program\n");
+            List<Contacts> addressBook = new List<Contacts>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter contact information:");
+
+                Console.Write("First Name: ");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Last Name: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Address: ");
+                string address = Console.ReadLine();
+
+                Console.Write("City: ");
+                string city = Console.ReadLine();
+
+                Console.Write("State: ");
+                string state = Console.ReadLine();
+
+                Console.Write("ZIP: ");
+                string zip = Console.ReadLine();
+
+                Console.Write("Phone Number: ");
+                string phoneNumber = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Contacts contact = new Contacts()
+                {
+                    firstName = firstName,
+                    lastName = lastName,
+                    address = address,
+                    city = city,
+                    state = state,
+                    zip = zip,
+                    phoneNumber = phoneNumber,
+                    email = email
+                };
+
+                addressBook.Add(contact);
+
+                Console.WriteLine("Contact created successfully!");
+                Console.WriteLine("\n");
+                Console.WriteLine("Address Book Entries:");
+                foreach (Contacts contactList in addressBook)
+                {
+                    contactList.DisplayContactInfo();
+                }
+            }
+              
+        }
+    }
+}
